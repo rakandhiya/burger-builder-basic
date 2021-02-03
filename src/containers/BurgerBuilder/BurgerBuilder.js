caf4acf003ed23faa03274d4fad82637360bd4e9
Rakandhiya Daanii Rachmanto <rakandhiya.daanii@gmail.com>
2021-02-03 18:45:58 +0700

    implement backdrop component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -92,6 +92,12 @@ class BurgerBuilder extends Component
         })
     }
 
+    purchaseCancelHandler = () => {
+        this.setState({
+            purchasing: false
+        })
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -103,7 +109,9 @@ class BurgerBuilder extends Component
 
         return (
             <Auxiliary>
-                <Modal show={this.state.purchasing}>
+                <Modal 
+                    show={this.state.purchasing}
+                    modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
